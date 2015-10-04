b6091e8912ac27608d938cc5b8ee8eee3e60532e
SpoKen <spoken.spok@gmail.com>
2015-10-04 23:50:51 +0200

    izi

--- hud.cpp
+++ hud.cpp
@@ -8,8 +8,8 @@ Hud::Hud(){
 Hud::~Hud() {}
 
 void Hud::start_bg(sf::RenderWindow& w){
+    s_starter.setPosition(w.getSize().x/2 - starter.getSize().x/2,w.getSize().y/2 - starter.getSize().y/2);
     w.draw(s_starter);
-
 }
 
 void Hud::gameover_bg(sf::RenderWindow& w){
